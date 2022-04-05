421f0c9c41cf3a9abea134762fa0c2870eb54f43
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-04-05 20:33:05 +0900

    220405_solve : 다트게임_yu
    220405_solve : 다트게임_yu

---
+++ 0405/Programmers/Lv1/다트게임_yu.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class lv1_다트게임 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] score = new int[3];
+
+		String[] dart = sc.next().split("");
+
+		int num = 0;
+		int idx = 0;
+
+		for (int i = 0; i < dart.length; i++) {
+
+			switch (dart[i]) {
+			case "S":
+				score[idx] = num;
+				idx++;
+				break;
+			case "D":
+				score[idx] = (int) Math.pow(num, 2);
+				idx++;
+				break;
+			case "T":
+				score[idx] = (int) Math.pow(num, 3);
+				idx++;
+				break;
+			case "*":
+				if (idx - 2 >= 0) {
+					score[idx - 2] *= 2;
+				}
+				score[idx - 1] *= 2;
+				break;
+			case "#":
+				score[idx - 1] *= -1;
+				break;
+			default:
+				if (dart[i].equals("1") && dart[i + 1].equals("0")) {
+					i++;
+					num= 10;
+					continue;
+					
+				} else {
+
+					num = Integer.parseInt(dart[i]);
+				}
+				break;
+			}// switch
+
+		} // for
+
+		int answer = 0;
+		for (int i = 0; i < 3; i++) {
+
+			answer += score[i];
+		}
+
+		System.out.println(answer);
+
+	}
+
+}
