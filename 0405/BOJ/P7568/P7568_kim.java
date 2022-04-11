5dcf0a44e41f81a5b8f4cedf8c4dbb94622ceb3e
노호준 <hh950131@gmail.com>
2022-04-11 21:35:08 +0900

    zzz

--- 0405/BOJ/P7568/P7568_kim.java
+++ 0405/BOJ/P7568/P7568_kim.java
@@ -7,22 +7,23 @@ public class BAEKJOON_P7568_덩치 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(); // 사람 수
 		int[][] size = new int[n][2];
-		for(int i=0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			size[i][0] = sc.nextInt(); // 몸무게
 			size[i][1] = sc.nextInt(); // 키
 		}
 		int check = 0;
-		while(check < n) {
+		while (check < n) {
 			int grade = 1;
-			for(int i = 0; i < n; i++) {
-				if(check == i) continue; // 나 자신은 비교할 필요가 없으니까
-				if(size[check][0] < size[i][0] && size[check][1] < size[i][1]) {
+			for (int i = 0; i < n; i++) {
+				if (check == i)
+					continue; // 나 자신은 비교할 필요가 없으니까
+				if (size[check][0] < size[i][0] && size[check][1] < size[i][1]) {
 					grade++;
 				}
 			}
-			System.out.print(grade+" ");
+			System.out.print(grade + " ");
 			check++;
-		}		
+		}
 	}
 
 }
