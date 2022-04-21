a41fd868d8b56e272cd728c4825989c69dc8a16a
codeehh <87011085+codeehh@users.noreply.github.com>
2022-04-21 20:24:46 +0900

    220421_solve : Lv2_noh

--- 0421/Programmers/Lv2/Lv2_noh.java
+++ 0421/Programmers/Lv2/Lv2_noh.java
@@ -1,32 +1,59 @@
-class Solution {
-    public int solution(String s) {
-        int slen = s.length();
-        int answer = slen;
+import java.util.Stack;
 
-        for (int i = 1; i <= slen / 2; ++i) {
-            int len = 0;
-            len += (slen % i);
-            String temp = s.substring(0, slen - (slen % i));
-            int tempLen = temp.length();
-            int cnt = 1;
-            for (int j = i; j < tempLen; j += i) {
-                // 이전 문자열과 같을때
-                if (temp.substring(j, j + i).equals(temp.substring(j - i, j))) {
-                    ++cnt;
-                }
-                // 다를때
-                else {
-                    len += (Integer.toString(cnt).length() + i);
-                    if (cnt == 1)
-                        --len;
-                    cnt = 1;
-                }
-            }
-            len += (Integer.toString(cnt).length() + i);
-            if (cnt == 1)
-                --len;
-            answer = Math.min(answer, len);
-        }
+class Solution {
+    public String solution(String p) {
+        String answer = "";
+        answer = solve(p);
         return answer;
+    }
+
+    static String solve(String str) {
+        // 빈 문자열이면 그대로 반환
+        if (str.equals(""))
+            return str;
+
+        // 문자열을 u, v로 분리
+        String u = new String();
+        String v = new String();
+        int cnt = 0;
+        for (int i = 0; i < str.length(); ++i) {
+            if (str.charAt(i) == '(')
+                ++cnt;
+            else
+                --cnt;
+            if (cnt == 0) {
+                u = str.substring(0, i + 1);
+                v = str.substring(i + 1, str.length());
+                break;
+            }
+        }
+
+        // u가 올바른 문자열이면
+        if (isRight(u)) {
+            return u + solve(v);
+        }
+        // u가 올바른 문자열이 아니면
+        else {
+            String temp = new String();
+            for (int i = 1; i < u.length() - 1; ++i)
+                if (u.charAt(i) == '(')
+                    temp += ')';
+                else
+                    temp += '(';
+            return "(" + solve(v) + ")" + temp;
+        }
+    }
+
+    static boolean isRight(String str) {
+        Stack<Character> st = new Stack<Character>();
+        for (int i = 0; i < str.length(); ++i)
+            if (str.charAt(i) == '(')
+                st.push('(');
+            else {
+                if (st.empty())
+                    return false;
+                st.pop();
+            }
+        return true;
     }
 }
