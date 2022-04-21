f492ace15ab01d59ade1bb9b008eaf4f5d023e6d
codeehh <87011085+codeehh@users.noreply.github.com>
2022-04-21 20:24:07 +0900

    220421_solve : Lv1_noh

--- 0421/Programmers/Lv1/Lv1_noh.java
+++ 0421/Programmers/Lv1/Lv1_noh.java
@@ -1,62 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public String solution(int[] numbers, String hand) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
+    static final String[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-        int[][] pos = { { 3, 1 }, { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
-                { 2, 2 } };
+    public int solution(String s) {
+        int answer = 0;
 
-        int[] leftPos = { 3, 0 };
-        int[] rightPos = { 3, 2 };
-        for (int i = 0; i < numbers.length; ++i) {
-            // 2580
-            if (numbers[i] % 3 == 2 || numbers[i] == 0) {
-                // 거리가 같을때
-                if (dist(leftPos, pos[numbers[i]]) == dist(rightPos, pos[numbers[i]])) {
-                    // 왼손잡이면
-                    if (hand.equals("left")) {
-                        sb.append("L");
-                        leftPos[0] = pos[numbers[i]][0];
-                        leftPos[1] = pos[numbers[i]][1];
-                    }
-                    // 오른손잡이면
-                    else {
-                        sb.append("R");
-                        rightPos[0] = pos[numbers[i]][0];
-                        rightPos[1] = pos[numbers[i]][1];
-                    }
-                }
-                // 왼손이 가까울때
-                else if (dist(leftPos, pos[numbers[i]]) < dist(rightPos, pos[numbers[i]])) {
-                    sb.append("L");
-                    leftPos[0] = pos[numbers[i]][0];
-                    leftPos[1] = pos[numbers[i]][1];
-                }
-                // 오른손이 가까울때
-                else if (dist(leftPos, pos[numbers[i]]) > dist(rightPos, pos[numbers[i]])) {
-                    sb.append("R");
-                    rightPos[0] = pos[numbers[i]][0];
-                    rightPos[1] = pos[numbers[i]][1];
-                }
+        Map<String, Integer> mp = new HashMap<String, Integer>();
+        for (int i = 0; i <= 9; ++i)
+            mp.put(numbers[i], i);
+
+        String temp = new String();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if ('0' <= c && c <= '9') {
+                answer *= 10;
+                answer += c - '0';
+                continue;
             }
-            // 147
-            else if (numbers[i] % 3 == 1) {
-                sb.append("L");
-                leftPos[0] = pos[numbers[i]][0];
-                leftPos[1] = pos[numbers[i]][1];
-            }
-            // 369
-            else if (numbers[i] % 3 == 0) {
-                sb.append("R");
-                rightPos[0] = pos[numbers[i]][0];
-                rightPos[1] = pos[numbers[i]][1];
+            temp += c;
+            if (mp.get(temp) != null) {
+                answer *= 10;
+                answer += mp.get(temp);
+                temp = new String();
             }
         }
-        answer = sb.toString();
-        return answer;
-    }
 
-    static int dist(int[] a, int[] b) {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+        return answer;
     }
 }
